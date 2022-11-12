2a7f54fee5e694c0f7a64b3cc00fe6006dc21c58
Gvidas Garadauskas <ga.gvidas@gmail.com>
2022-11-12 22:26:28 +0200

    assignment

---
+++ First_task/Program.cs
@@ -0,0 +1,101 @@
+﻿/*
+    REIZ TECH HOMEWORK ASSIGNMENT PROGRAM FOR THE .NET DEVELOPER INTERN POSITION
+    1st task
+    Gvidas Garadauskas
+*/
+
+using System;
+
+namespace First_task
+{
+    /// <summary>
+    /// Class of the program which allows user to input the time of a clock
+    /// through console and outputs lesser angle in degrees between arrows
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// The first method that is invoked
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            InOutUtils.PrintAngle(TaskUtils.CalculateLesserAngle(InOutUtils.ReadHours(), InOutUtils.ReadMinutes()));
+        }
+    }
+
+    /// <summary>
+    /// Class for calculations
+    /// </summary>
+    static class TaskUtils
+    {
+        /// <summary>
+        /// Method which calculates lesser angle between specified time arrows
+        /// </summary>
+        /// <param name="hours">hours parameter</param>
+        /// <param name="minutes">minutes parameter</param>
+        /// <returns>lesser angle in degrees between arrows</returns>
+        public static double CalculateLesserAngle(int hours, int minutes)
+        {
+            double hoursAngle = 360 / 12 * hours + 0.5 * minutes; //angle every hour increases by 30 degrees + 0.5 degrees for each minute
+            double minutesAngle = 360 / 60 * minutes; //angle every minute increases by 6 degrees
+
+            double lesserAngle = Math.Abs(hoursAngle - minutesAngle);
+            if (Math.Abs(minutesAngle - hoursAngle) < lesserAngle) lesserAngle = Math.Abs(minutesAngle - hoursAngle);
+            if (lesserAngle > 180) lesserAngle = 360 - lesserAngle; //prefer lesser angle
+
+            return lesserAngle;
+        }
+    }
+
+    /// <summary>
+    /// Class for inputing and outputing data
+    /// </summary>
+    static class InOutUtils
+    {
+        /// <summary>
+        /// Method which allows user to enter hours to a console
+        /// </summary>
+        /// <returns>entered hours</returns>
+        public static int ReadHours()
+        {
+            Console.WriteLine("Enter hours of the analogue clock:");
+            int inputHours = int.Parse(Console.ReadLine());
+
+            if (inputHours < 1 || inputHours > 12)
+            {
+                Console.WriteLine("Incorrect hours of the analogue clock! Hours must be 1 to 12.");
+                inputHours = ReadHours();
+            }
+
+            return inputHours;
+        }
+
+        /// <summary>
+        /// Method which allows user to enter minutes to a console
+        /// </summary>
+        /// <returns>entered minutes</returns>
+        public static int ReadMinutes()
+        {
+            Console.WriteLine("Enter minutes of the analogue clock:");
+            int inputMinutes = int.Parse(Console.ReadLine());
+
+            if (inputMinutes < 0 || inputMinutes > 59)
+            {
+                Console.WriteLine("Incorrect minutes of the analogue clock! Minutes must be from 0 to 59.");
+                inputMinutes = ReadMinutes();
+            }
+
+            return inputMinutes;
+        }
+
+        /// <summary>
+        /// Method which outputs results data
+        /// </summary>
+        /// <param name="angle">lesser angle between specified time arrows</param>
+        public static void PrintAngle(double angle)
+        {
+            Console.WriteLine("Lesser angle in degrees between hours arrow and minutes arrow: " + angle); 
+        }
+    }
+}
---
+++ Second_task/Program.cs
@@ -0,0 +1,126 @@
+﻿/*
+    REIZ TECH HOMEWORK ASSIGNMENT PROGRAM FOR THE .NET DEVELOPER INTERN POSITION
+    2nd task
+    Gvidas Garadauskas
+*/
+
+using System;
+using System.Collections.Generic;
+
+namespace Second_task
+{
+    /// <summary>
+    /// Class of the program which is capable of printing specified tree (sub-tree)
+    /// and finding its depth
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// The first method that is invoked
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            //test variant - a tree which has 5 levels
+            
+            Branch<string> root = new Branch<string>("root"); //an object of a tree (root) which has its sub-trees (branches)
+            {
+                Branch<string> branch1 = root.AddChildBranch("branch1");
+                Branch<string> branch2 = root.AddChildBranch("branch2");
+                Branch<string> branch3 = root.AddChildBranch("branch3");
+                {
+                    Branch<string> branch31 = branch3.AddChildBranch("branch31");
+                    Branch<string> branch32 = branch3.AddChildBranch("branch32");
+                    {
+                        Branch<string> branch321 = branch32.AddChildBranch("branch321");
+                        Branch<string> branch322 = branch32.AddChildBranch("branch322");
+                    }
+                }
+                Branch<string> branch4 = root.AddChildBranch("branch4");
+                {
+                    Branch<string> branch41 = branch4.AddChildBranch("branch41");
+                }
+                Branch<string> branch5 = root.AddChildBranch("branch5");
+                {
+                    Branch<string> branch51 = branch5.AddChildBranch("branch51");
+                    {
+                        Branch<string> branch511 = branch51.AddChildBranch("branch511");
+                        {
+                            Branch<string> branch5111 = branch511.AddChildBranch("branch5111");
+                        }
+                    }
+                }
+            }
+
+            root.PrintTree(root, "");
+            int depth = root.CalculateDepth(root);
+            Console.WriteLine("Depth of provided structure: " + depth);
+        }
+
+        /// <summary>
+        /// The class of a tree (sub-tree)
+        /// </summary>
+        /// <typeparam name="T">type parameter</typeparam>
+        class Branch<T>
+        {
+            List<Branch<T>> branches;
+
+            public T value;
+            public Branch<T> parent;
+
+            /// <summary>
+            /// Constructor of a tree (or its branch)
+            /// </summary>
+            /// <param name="data">data parameter</param>
+            public Branch(T data)
+            {
+                this.value = data;
+                this.parent = null;
+                this.branches = new List<Branch<T>>();
+            }
+
+            /// <summary>
+            /// Method which adds a sub-tree to tree
+            /// </summary>
+            /// <param name="value">data parameter</param>
+            /// <returns></returns>
+            public Branch<T> AddChildBranch(T value)
+            {
+                Branch<T> childBranch = new Branch<T>(value);
+                childBranch.parent = this;
+                branches.Add(childBranch);
+                return childBranch;
+            }
+
+            /// <summary>
+            /// Recursive Method which calculates the depth of tree (sub-tree)
+            /// </summary>
+            /// <param name="tree">specified tree</param>
+            /// <param name="depth">integer which pluses by one after new level</param>
+            /// <returns>specified tree depth</returns>
+            public int CalculateDepth(Branch<T> tree, int depth = 0)
+            {
+                foreach (var child in tree.branches)
+                {
+                    CalculateDepth(child, depth++); //recursion
+                }
+                return depth;
+            }
+
+            /// <summary>
+            /// Recursive Method which prints the tree (sub-tree) to a console
+            /// </summary>
+            /// <param name="tree">specified tree</param>
+            /// <param name="levelString">string to help visualise tree </param>
+            public void PrintTree(Branch<T> tree, string levelString)
+            {
+                Console.WriteLine(levelString + " " + tree.value);
+
+                foreach (var child in tree.branches)
+                {
+                    PrintTree(child, levelString + "  "); //recursion
+                }
+            }
+        }
+    }
+}
